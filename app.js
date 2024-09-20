c15161fec17684e12d37027e0041e85fba1fabef
Thhiago ツ <157834329+ThhiagoCarvalho@users.noreply.github.com>
2024-09-20 15:39:24 -0300

    INICIO

--- app.js
+++ app.js
@@ -4,12 +4,42 @@ const portaServico = 3000;
 
 app.use(express.json());
 
-app.get ('/' , (request , response) =>{
+
+app.get ('/usuarios' , (request , response) =>{
     const resposta =  {msg : "Ola mundo"}
     response.status(200).send(resposta)
 })
 
 
-app.listen(portaServico, () =>{;
+app.get ('/usuarios/:id' , (request , response) =>{
+    const parametroId = request.params.id
+    const resposta =  {msg : `Ola ! recebi um id: ${parametroId} `}
+    response.status(200).send(resposta)
+})
+
+
+app.post ('/usuarios' , (request , response) =>{
+    const resposta =  {msg : "recebi um POST"}
+    response.status(200).send(resposta)
+})
+
+
+app.put ('/usuarios/:id' , (request , response) =>{
+    const parametroId = request.params.id
+    const resposta =  {msg : "recebi um PUT"}
+    response.status(200).send(resposta)
+})
+
+
+app.delete ('/usuarios/:id' , (request , response) =>{
+    const parametroId = request.params.id
+    const resposta =  {msg : "recebi um DELETE"}
+    response.status(200).send(resposta)
+})
+
+
+
+
+app.listen(portaServico, () =>{
     console.log("Api rodando na porta " + portaServico);
 })
